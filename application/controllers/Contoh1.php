9da4e6255eb6d2b8e87bed7ce0eb1b99ba4326de
Ignasius-seriang123 <allseriang65@gmail.com>
2022-10-03 17:59:53 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya ignasius Ardilos Seriang 
+        saya tinggal di daerah puri gading 
+        olahraga yang saya sukai adalah futsal";
+    }
+}
+?>
---
+++ application/views/Home.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Halaman Awal dengan Codeigniter</title>
+    </head>
+    <body>
+        <h1>Selamat datang. Ini Halaman Awal</h1>
+    </body>
+</html>
